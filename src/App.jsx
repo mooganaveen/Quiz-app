876440fbcbeb7ec369e649236180db535029a7d5
mooganaveen <mooganaveensharma@gmail.com>
2024-07-15 22:20:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import Quiz from './components/quiz/quiz/'
+
+const App = () => {
+  return (
+    <>
+      <Quiz/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/quiz/quiz.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './quiz.css'
+
+const Quiz = () => {
+  return (
+    <div className='container'>
+        <h1>Quiz App</h1>
+        <h2>Which button is used for the Internet Connection?</h2>
+        <ul>
+            <li>Button 1</li>
+            <li>Button 2</li>
+            <li>Button 3</li>
+        </ul>
+      
+    </div>
+  )
+}
+
+export default Quiz
